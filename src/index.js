f790714c9cadc4852d84bf98fc0e028fed6d723d
Vitya <sint9ra@gmail.com>
2026-05-16 21:31:35 +0700

    added changes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import _ from 'lodash';
 
-export default () => {
-  console.log('Hello, Hexlet!');
-  console.log(_.last(['one', 'two']));
+const capitalize = (text) => {
+  if (text === '') {
+    return '';
+  }
+  const firstChar = text[0].toUpperCase();
+  const restSubstring = text.slice(1);
+  return `${firstChar}${restSubstring}`;
 };
+
+// Экспортируем функцию, чтобы её можно было импортировать в тесте
+export default capitalize;
